0b5df23305672ff7f26793288f12992dcd73f93c
Victor Queiroz <victorqueiroz.ti@gmail.com>
2024-10-17 09:31:32 -0300

    feat: Add Exception Handler to manage Database and SQL exceptions

--- src/main/java/com/caloteiros/spring/config/GlobalExceptionHandler.java
+++ src/main/java/com/caloteiros/spring/config/GlobalExceptionHandler.java
@@ -1,19 +1,41 @@
 package com.caloteiros.spring.config;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.dao.DataAccessException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import java.sql.SQLException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public String handleGeneralException(Exception ex, Model model) {
+    public ModelAndView handleGeneralException(Exception ex, Model model) {
+        ModelAndView mv = new ModelAndView("error/error-default");
         model.addAttribute("error", "Erro");
         model.addAttribute("message", "Ocorreu um erro no servidor.");
-        return "error/error-default";
+        return mv;
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ModelAndView handle404(NoResourceFoundException ex) {
+        ModelAndView mv = new ModelAndView("error/error-404");
+        mv.addObject("error", "Página não encontrada");
+        mv.addObject("message", "A URL solicitada não existe.");
+        return mv;
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    public ModelAndView handleDatabaseException(DataAccessException ex) {
+        ModelAndView mv = new ModelAndView("error/error-sql");
+        mv.addObject("error", "SQL Error");
+        mv.addObject("message", "Erro com a operação no banco de dados, verifique a transação.");
+        return mv;
     }
 
 }
---
+++ src/main/webapp/WEB-INF/views/error/error-sql.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ include file="/WEB-INF/views/includes/includes.jsp" %>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página de Erro</title>
+    <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/css/error.css">
+</head>
+<body>
+	<header>
+		<c:import url="${pageContext.request.contextPath}/WEB-INF/views/includes/header.jsp" />
+		<h2>Caloteiros</h2>
+	</header>
+    <main>
+        <h1>${error}</h1>
+        <p>${message}</p>
+        <a href="/menu">Retornar ao Menu</a>
+    </main>
+    <footer>
+       <c:import url="${pageContext.request.contextPath}/WEB-INF/views/includes/footer.jsp" />
+    </footer>
+</body>
+</html>
